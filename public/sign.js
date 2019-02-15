3f470f871e90da3db82de27ad46693e9d7ac8518
Your Name Here <millie@mcquillin.com>
2019-02-15 20:52:35 +0000

    changesMade

--- public/sign.js
+++ public/sign.js
@@ -7,12 +7,12 @@ var vertexShaderText =
 'varying vec2 fragTexCoord;',
 'uniform mat4 mWorld;',
 'uniform mat4 mView;',
-'uniform mat4 mProj;',
+'uniform mat4 u_ProjMatrix;',
 '',
 'void main()',
 '{',
 '  fragTexCoord = vertTexCoord;',
-'  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
+'  gl_Position = u_ProjMatrix * mView * mWorld * vec4(vertPosition, 1.0);',
 '}'
 ].join('\n');
 
