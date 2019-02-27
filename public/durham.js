f73a9a3015bb58ede111961a8907fe9dbbd842a4
Your Name Here <millie@mcquillin.com>
2019-02-27 10:48:49 +0000

    Slight edit VS - still works

--- public/durham.js
+++ public/durham.js
@@ -4,6 +4,7 @@ var VSHADER_SOURCE =
   'attribute vec3 a_Position;\n' +
   'attribute vec4 a_Color;\n' +
   'attribute vec4 a_Normal;\n' +        // Normal
+  'attribute vec2 a_TexCoords;\n' +
   'uniform mat4 u_ModelMatrix;\n' +
   'uniform mat4 u_NormalMatrix;\n' +
   'uniform mat4 u_ViewMatrix;\n' +
@@ -11,9 +12,11 @@ var VSHADER_SOURCE =
   'uniform vec3 u_LightColor;\n' +     // Light color
   'uniform vec3 u_LightDirection;\n' + // Light direction (in the world coordinate, normalized)
   'varying vec4 v_Color;\n' +
+  'varying vec2 v_TexCoords;\n' +
   'uniform bool u_isLighting;\n' +
   'void main() {\n' +
   '  gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * vec4(a_Position, 1.0);\n' +
+  '  v_TexCoords = a_TexCoords;\n' +
   '  if(u_isLighting)\n' + 
   '  {\n' +
   '     vec3 normal = normalize((u_NormalMatrix * a_Normal).xyz);\n' +
@@ -25,6 +28,7 @@ var VSHADER_SOURCE =
   '  {\n' +
   '     v_Color = a_Color;\n' +
   '  }\n' + 
+
   '}\n';
 
 // Fragment shader program
