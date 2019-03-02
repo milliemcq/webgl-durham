947a43d657f43c1919ef707a762a5d761bafc418
Your Name Here <millie@mcquillin.com>
2019-03-02 10:26:07 +0000

    Sunlight changed

--- public/durham.js
+++ public/durham.js
@@ -117,7 +117,7 @@ function main(buildingModel) {
   }
 
   // Set the light color (white)
-  gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
+  gl.uniform3f(u_LightColor, 0.7, 0.5, 0.5);
   // Set the light direction (in the world coordinate)
   var lightDirection = new Vector3([0.5, 3.0, 4.0]);
   lightDirection.normalize();     // Normalize
@@ -710,6 +710,18 @@ function draw(gl, u_ModelMatrix, u_NormalMatrix, u_isLighting, buildingModel) {
     drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
   modelMatrix = popMatrix();
 
+  var n = sphere(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  pushMatrix(modelMatrix);
+    modelMatrix.translate(0, 0, 0);
+    modelMatrix.scale(1, 1, 1); // Scale
+    drawbox(gl, u_ModelMatrix, u_NormalMatrix, n);
+  modelMatrix = popMatrix();
+
   /*
   //CREATE THE CYLINDER
   var n = cylinder(gl);
@@ -947,3 +959,55 @@ function Cylinder () {
       normals: normals,
     };
   };
+
+  function sphere(gl)
+    {
+      var SPHERE_DIV = 6;
+      var i, ai, si, ci;
+      var j, aj, sj, cj;
+      var p1, p2;
+      var vertices = [],indices = [];
+      for (j = 0; j <= SPHERE_DIV; j++) 
+      {
+        aj = j * Math.PI / SPHERE_DIV;
+        sj = Math.sin(aj);
+        cj = Math.cos(aj);
+        for (i = 0; i <= SPHERE_DIV; i++) 
+        {
+          ai = i * 2 * Math.PI / SPHERE_DIV;
+          si = Math.sin(ai);
+          ci = Math.cos(ai);
+          vertices.push(si * sj);  // X
+          vertices.push(cj);       // Y
+          vertices.push(ci * sj);  // Z
+        }
+      }
+
+      for (j = 0; j < SPHERE_DIV; j++)
+      {
+        for (i = 0; i < SPHERE_DIV; i++)
+        {
+          p1 = j * (SPHERE_DIV+1) + i;
+          p2 = p1 + (SPHERE_DIV+1);
+          indices.push(p1);
+          indices.push(p2);
+          indices.push(p1 + 1);
+          indices.push(p1 + 1);
+          indices.push(p2);
+          indices.push(p2 + 1);
+        }
+      }
+      var vertexBuffer = gl.createBuffer();
+      if (!vertexBuffer) 
+      {
+        console.log('Failed to create the buffer object');
+        return -1;
+      }
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); 
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW); 
+      var indexBuffer = gl.createBuffer(); 
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer); 
+      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+      return indices.length;
+    }
